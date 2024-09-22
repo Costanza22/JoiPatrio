f0b2bb0778109eb12eb9e2902b64ba966599981d
Costanza Pasquotto Assef <81304267+Costanza22@users.noreply.github.com>
2024-09-22 13:49:32 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Navbar from './Navbar';
 import './App.css';
 import { FaRegNewspaper } from "react-icons/fa";
 import { SiMicrodotblog } from "react-icons/si";
+import { BsFillHousesFill } from "react-icons/bs";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [videoUrl, setVideoUrl] = useState('');
+  const [videoPlaying, setVideoPlaying] = useState(false); // Estado para controle de reprodução
+
   const images = [
     'https://omunicipiojoinville.com/wp-content/uploads/2023/03/palacete-doria-joinville.jpg',
     'https://facts.net/wp-content/uploads/2023/07/48-facts-about-joinville-1688694462.jpeg',
@@ -20,19 +26,26 @@ function App() {
     return () => clearInterval(interval);
   }, [images.length]);
 
-  const [videoUrl, setVideoUrl] = useState('');
-
   const handleImageClick = (url) => {
     setVideoUrl(url);
   };
 
   const handleCloseVideo = () => {
     setVideoUrl('');
+    setVideoPlaying(false); // Reseta a reprodução ao fechar o vídeo
+  };
+
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
+
+  const handlePlayVideo = () => {
+    setVideoPlaying(true);
   };
 
   return (
     <div className="App">
-      <Navbar />
+      <Navbar openModal={openModal} />
+      
       <div className="carousel-container">
         <div className="carousel">
           <div 
@@ -59,6 +72,10 @@ function App() {
 
       <main>
         <section className="posts">
+          <div className="title-container">
+            <BsFillHousesFill size={30} />
+            <h1 className="posts-title">Demonstração</h1>
+          </div>
           <div className="post" onClick={() => handleImageClick('https://www.youtube.com/embed/QTrPPoBk4Lo?start=57')}>
             <img src="https://cortinadopassado.com.br/wp-content/uploads/2019/03/big_53d49eeec79d01cf7030dc5693f3deea.jpg?w=525" alt="Fashion" />
             <h2>Casa Kruger</h2>
@@ -79,58 +96,78 @@ function App() {
         {videoUrl && (
           <div className="video-popup">
             <button className="close-button" onClick={handleCloseVideo}>X</button>
-            <iframe
-              width="560"
-              height="315"
-              src={videoUrl}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded YouTube Video"
-            ></iframe>
+            {!videoPlaying ? (
+              <div className="video-cover" onClick={handlePlayVideo}>
+                <img src="https://i.ytimg.com/vi/tN3Q6qOvTow/maxresdefault.jpg" alt="Capa do vídeo" />
+                <div className="play-button">▶</div>
+              </div>
+            ) : (
+              <iframe
+                width="560"
+                height="315"
+                src={videoUrl}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded YouTube Video"
+              ></iframe>
+            )}
           </div>
         )}
 
-        <section className="news-container">
-          <div className="news">
-            <h2><FaRegNewspaper /> 14/08/2024 08:33</h2>
-            <p>Na madrugada desta quarta-feira (14), um incêndio atingiu um casarão abandonado no Centro de Joinville. De acordo com informações, o imóvel era frequentemente utilizado por pessoas em situação de rua. Felizmente, não houve registro de feridos. A operação de combate ao fogo mobilizou 16 bombeiros e cinco viaturas, sendo necessários cerca de 40 mil litros de água para controlar as chamas que consumiram todos os cômodos da edificação.</p>
-            <a href="https://diarinho.net/materia/655317/Incendio-criminoso-destroi-casarao-historico-de-Joinville" target="_blank" rel="noopener noreferrer">
-              <img 
-                src="https://scc10.com.br/wp-content/uploads/2024/08/Fogo-atinge-casarao-abandonado-em-Joinville-veja-imagens-1-e1723634315373.jpeg.webp" 
-                alt="Bombeiros combatendo um incêndio em um casarão abandonado em Joinville" 
-                width="500" 
-              />
-            </a>
-          </div>
+<section className="news-container">
+  <div className="title-container">
+    <FaRegNewspaper size={30} />
+    <h1 className="news-title">Notícias</h1>
+  </div>
+  
+  <div className="news">
+    <h2>14/08/2024 08:33</h2>
+    <p>Na madrugada desta quarta-feira (14), um incêndio atingiu um casarão abandonado no Centro de Joinville. De acordo com informações, o imóvel era frequentemente utilizado por pessoas em situação de rua. Felizmente, não houve registro de feridos. A operação de combate ao fogo mobilizou 16 bombeiros e cinco viaturas, sendo necessários cerca de 40 mil litros de água para controlar as chamas que consumiram todos os cômodos da edificação.</p>
+    <a href="https://diarinho.net/materia/655317/Incendio-criminoso-destroi-casarao-historico-de-Joinville" target="_blank" rel="noopener noreferrer">
+      <img 
+        src="https://www.joinville.sc.gov.br/wp-content/uploads/2023/02/casarao-800x533.jpeg" 
+        alt="Bombeiros combatendo um incêndio em um casarão abandonado em Joinville" 
+        width="500" 
+        style={{ border: '5px solid black', borderRadius: '8px' }} 
+      />
+    </a>
+  </div>
 
-          <div className="news">
-            <h2><FaRegNewspaper /> 13/09/2024 às 19h26</h2>
-            <p>Uma casa enxaimel que faz parte da lista de imóveis com proteção cultural de Joinville, no Norte de Santa Catarina, foi atingida por um incêndio na noite desta sexta-feira (13). A casa faz parte da lista de imóveis com proteção cultural em Joinville. Corpo de Bombeiros Voluntários foi acionado às 19h05 para conter as chamas na casa que fica na rua Ottoker Doerffel, no bairro Atiradores. O fogo se alastrou rapidamente, chegando perto da rede elétrica e ameaçando outros imóveis, como uma clínica dentária.</p>
-            <a href="https://omunicipiojoinville.com/incendio-atinge-casa-enxaimel-no-bairro-anita-garibaldi-em-joinville/" target="_blank" rel="noopener noreferrer">
-              <img 
-                src="https://omunicipiojoinville.com/wp-content/uploads/2024/09/imagem-destacada-fred-5-1068x580.jpg" 
-                alt="Bombeiros combatendo um incêndio em um casarão abandonado em Joinville" 
-                width="500" 
-              />
-            </a>
+  <div className="news">
+    <h2>13/09/2024 às 19h26</h2>
+    <p>Uma casa enxaimel que faz parte da lista de imóveis com proteção cultural de Joinville, no Norte de Santa Catarina, foi atingida por um incêndio na noite desta sexta-feira (13). O Corpo de Bombeiros Voluntários foi acionado às 19h05 para conter as chamas.
+    O Corpo de Bombeiros Voluntários foi acionado às 19h05 para conter as chamas na casa que fica na rua Ottoker Doerffel, no bairro Atiradores. O fogo se alastrou rapidamente, chegando perto da rede elétrica e ameaçando outros imóveis, como uma clínica dentária.
+    </p>
+    <a href="https://omunicipiojoinville.com/incendio-atinge-casa-enxaimel-no-bairro-anita-garibaldi-em-joinville/" target="_blank" rel="noopener noreferrer">
+      <img 
+        src="https://th.bing.com/th/id/R.9929cfdbdd5f90d07e74436a43078a80?rik=IILc0%2fWK7NvRjQ&pid=ImgRaw&r=0" 
+        alt="Incêndio em um casarão" 
+        width="500" 
+        style={{ border: '5px solid black', borderRadius: '8px' }} 
+      />
+    </a>
+  </div>
+</section>
+{/* Nova seção de blog */}
+<section className="blog-container">
+          <div className="title-container">
+            <SiMicrodotblog size={30} />
+            <h1 className="blog-title">Blog sobre Patrimônio Histórico</h1>
           </div>
-        </section>
-
-        <section className="description-container">
-          <h2><SiMicrodotblog /> Blog</h2>
-          <h1>JOINVILLE EM FOCO</h1>
-          <p>Bem-vindo ao Joinville em Foco, um blog dedicado a explorar a vida diária da maior cidade de Santa Catarina e destacar os seus aspectos únicos e vibrantes. Conhecida por sua rica história, economia dinâmica e forte tradição cultural, Joinville, muitas vezes chamada de "Cidade das Flores" e "Manchester Catarinense", possui um cenário urbano que mistura modernidade e tradição. Nosso objetivo é capturar essa essência e compartilhá-la com você.</p>
-          <div className="description-images">
-            <img 
-              src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg-0g2vDgovt2JZe8RIOnI9KvS0a-5PX6dBruDyetnXZdmFVRl9Y3qJH_q7EzGF4qgXwDGSAmtQAJj-97XujVLVrqQgRShemai4BsfiUXUkMjnmT87wG4i7RMkbLMgR_tzczFqnlLNl7ighXRkYp6a8MZiwdQNfoDu3fpNut5nu7mVn7mdspA/s4160/IMG_20220527_143038816_HDR.jpg" 
-              width="500" 
-              style={{ marginRight: '20px' }}
-            />
-            <img 
-              src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhY6fums2NtBmL22EveOoqEy9t2dQ5K5vlGHgkKqIFe2Jj-4iyPXS_Vk0_mOkhQ17oz9VF3jN5XeGoXvZWq7GGCmNAKlLudjd6yqNNov7TQ79HuUZv0liHkcxYtBaJlokMLu483oVJ0pQzfA8nsq_7xNn_jo9ludQY_FXnZkv2DMNN2x8Y9Zg/s4160/IMG_20220527_141343639.jpg" 
-              width="500"
-            />
+          <div className="blog-posts">
+            <article className="blog-post">
+              <h2>A importância de preservar os casarões antigos</h2>
+              <p>Os casarões históricos são marcos da história de Joinville e representam a cultura e a memória da cidade. Preservá-los é essencial para manter viva essa herança.</p>
+            </article>
+            <article className="blog-post">
+              <h2>Casarões mais emblemáticos de Joinville</h2>
+              <p>Alguns dos casarões mais conhecidos incluem o Palacete Niemeyer, a Casa da Memória, e o Museu Nacional de Imigração e Colonização.</p>
+            </article>
+            <article className="blog-post">
+              <h2>Desafios na preservação do patrimônio histórico</h2>
+              <p>A falta de investimentos e a ação do tempo são os principais desafios enfrentados na preservação desses imóveis, além do risco de incêndios.</p>
+            </article>
           </div>
         </section>
       </main>
